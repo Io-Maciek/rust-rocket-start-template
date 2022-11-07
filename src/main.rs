7e60d30965d3d4ff41d63bc32106efba8f750f83
Maciej Kuchcik <mkuchcik@gmail.com>
2022-11-07 14:59:26 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::{Build, Rocket};
+use rocket_dyn_templates::{context, Template};
+
+#[get("/")]
+fn index() -> Template {
+	Template::render("index", context! {
+				title: "Title",
+			})
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+	let figment = rocket::Config::figment().merge(("address", "0.0.0.0"));
+
+	rocket::custom(figment).mount("/", routes![index])
+		.attach(Template::fairing())
+}
